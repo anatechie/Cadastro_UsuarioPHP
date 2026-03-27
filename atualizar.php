e44bc9c79ccad6919d86220f8117b47cfbbd11ac
anatechie <anacaroline080812@gmail.com>
2026-03-27 10:30:01 -0300

    add projeto php

--- atualizar.php
+++ atualizar.php
@@ -20,3 +20,50 @@ if(!empty($_GET['cod'])){
     }
 }
 ?>
+
+<!doctype html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+    <h2>Atualizar dados do Usuário</h2>
+    <br>
+    <br>
+
+
+<form class="row g-3" action="update.php" method="post">
+  <div class="col-12">
+    <label for="inputNome" class="form-label">Nome</label>
+    <input type="text" class="form-control" id="inputNome" name="<?php echo $nome?>">
+  </div>
+
+
+  <div class="col-md-6">
+    <label for="inputEmail4" class="form-label">Email</label>
+    <input type="email" class="form-control" id="inputEmail" name="<?php echo $email?>">
+  </div>
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">Password</label>
+    <input type="password" class="form-control" id="inputPassword" name="<?php echo $senha?>">
+  </div>
+  
+  
+  <div class="col-md-2">
+    <label for="inputZip" class="form-label">Data Cadastro</label>
+    <input type="date" class="form-control" id="inputZip" name="<?php echo $data_cadastro?>">
+  </div>
+
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary">Atualizar</button>
+    <input type="hidden" name="cod" value = "<?php echo $codigo ?>">
+  </div>
+</form>
+
+</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
---
+++ update.php
@@ -0,0 +1,13 @@
+<?php
+require('conexao.php');
+
+if(isset($_POST['cod'])){
+    $codigo = $_POST['cod'];
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $data_cadastro = $_POST['data_cadastro'];
+
+    $sql = "update usuarios set nome = '$nome', senha ='$senha', data_cadastro = '$data_cadastro'";
+
+}
